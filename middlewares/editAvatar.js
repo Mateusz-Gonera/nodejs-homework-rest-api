53a7d3e0c8f341813dc94ef5e72305c2702ca4ed
Mateusz-Gonera <mateuszg1992@wp.pl>
2023-01-26 23:54:26 +0100

    Update editAvatar.js

--- middlewares/editAvatar.js
+++ middlewares/editAvatar.js
@@ -3,8 +3,8 @@ const Jimp = require("jimp");
 const editAvatar = () => {
   return (req, res, next) => {
     Jimp.read(req.file.path)
-        .then((img) => img.resize(250, 250).write(req.file.path))
-        .catch((err) => console.log(err.message));
+      .then((img) => img.resize(250, 250).write(req.file.path))
+      .catch((err) => console.log(err.message));
     next();
   };
 };
